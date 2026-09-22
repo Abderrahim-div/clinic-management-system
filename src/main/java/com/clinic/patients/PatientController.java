cc5e8c9c5de0b3ce5d19ed45a22c3c8105d8777e
Abderrahim Sbaia <sbaiaabderrahim1@gmail.com>
2026-09-22 16:52:05 +0100

    Work on controler GetMapping (findAll & findById)

--- src/main/java/com/clinic/patients/PatientController.java
+++ src/main/java/com/clinic/patients/PatientController.java
@@ -1,5 +1,29 @@
 package com.clinic.patients;
 
-public class PatientController {
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.clinic.patients.PatientRepository;
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController{
+    private PatientRepository patientRepository;
+    public PatientController(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
     
-}
+    @GetMapping
+    public List<Patient> findAll() {
+        return patientRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Patient findById(@PathVariable Long id){
+        return patientRepository.findById(id).orElseThrow(() -> new RuntimeException("Patient not found"));
+    }
+}   
